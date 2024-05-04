337a464bcc835c96e265a28b187f1ecebf778494
Phanurat <paunavt5@gmail.com>
2024-05-04 17:33:02 +0700

    Add SetUp files

---
+++ setup.bat
@@ -0,0 +1,17 @@
+npm i
+
+pip install bs4
+
+pip install requests
+
+pip install selenium
+
+pip install pyppeteer npm install hapi or npm install @hapi/hapi
+
+pip install pythainlp
+
+npm install puppeteer minify-html
+
+npm install axios
+
+npm install html-minifier
