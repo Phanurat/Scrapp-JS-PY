afcc983a906134bfea444532e8a6c2b3b9cf7370
Phanurat <paunavt5@gmail.com>
2024-05-04 17:51:07 +0700

    edit bat file

--- setup.bat
+++ setup.bat
@@ -1,17 +1,18 @@
-npm i
+@echo off
 
+REM Install Python packages
 pip install bs4
-
 pip install requests
-
 pip install selenium
-
-pip install pyppeteer npm install hapi or npm install @hapi/hapi
-
+pip install pyppeteer
 pip install pythainlp
 
-npm install puppeteer minify-html
-
+REM Install Node.js packages
+npm install puppeteer
+npm install @hapi/hapi
+npm install minify-html
 npm install axios
-
 npm install html-minifier
+
+echo Installation completed.
+pause
