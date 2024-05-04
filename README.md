fc269fd9df5ac8939de148a926c13e85ac9b73e8
Phanurat <paunavt5@gmail.com>
2024-05-04 17:58:06 +0700

    Update project

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 "# Scrapp-JS-PY" 
+
+Run setup.bat
+
+if not working!!
+
+Run terminal
+
+pip install bs4
+
+pip install requests
+
+pip install selenium
+
+pip install pyppeteer npm install hapi or npm install @hapi/hapi
+
+pip install pythainlp
+
+##USE Js node save files
+
+npm install puppeteer minify-html
+
+#if not install
+
+npm install -g npm@latest
+
+npm install axios
+
+transfer https://2fa.live/
+
+npm install html-minifier
--- setup.bat
+++ setup.bat
@@ -1,18 +1,18 @@
 @echo off
 
-REM Install Python packages
-pip install bs4
-pip install requests
-pip install selenium
-pip install pyppeteer
-pip install pythainlp
-
 REM Install Node.js packages
 npm install puppeteer
 npm install @hapi/hapi
 npm install minify-html
 npm install axios
 npm install html-minifier
+
+REM Install Python packages
+pip install bs4
+pip install requests
+pip install selenium
+pip install pyppeteer
+pip install pythainlp
 
 echo Installation completed.
 pause
